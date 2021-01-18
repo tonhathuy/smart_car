d8a343eaa2b4c1b5f924b2c3d9f7b1b005b0ab63
tonhathuy <tonhathuy97@gmail.com>
2021-01-18 12:04:37 +0700

    fix bug

--- BOT/api_tts/tts.py
+++ BOT/api_tts/tts.py
@@ -14,7 +14,13 @@ import time
 
 OUTPUT_MP3_FOLDER = 'BOT/api_tts/mp3_file/'
 API_KEY = 'ANhnbSnpQOrYnu4rQ5PRwYM90BWsjGN4'
+# API_KEY = 'UN99X7TICPsd5XhHt9AMDgLloBKSVTMC'
 
+def check_time_sleep(payload):
+    len_text = len(payload.split(' '))
+    if len_text == 1:
+        return int(len_text)
+    return int(len_text/2)
 
 def tts(payload):
     t1 = time.time()
@@ -24,6 +30,7 @@ def tts(payload):
         'speed': '',
         'voice': 'thuminh'
     }
+
 
     response = requests.request('POST', url, data=payload.encode('utf-8'), headers=headers).json()
     # response = {'async': 'https://static.openfpt.vn/text2speech-v5/short/2021-01-15/thuminh.0.b2389f4f086b3c1ae301029eeba0645a.mp3', 'error': 0, 'message': 'The content will be returned after a few seconds under the async link.', 'request_id': '8451557935db92fdf89fe6befe1c2f2f'}
@@ -38,7 +45,8 @@ def tts(payload):
         output_mp3_file = os.path.join(OUTPUT_MP3_FOLDER, mp3_file_name)
 
         http = urllib3.PoolManager()
-        time.sleep(1)
+        
+        time.sleep(check_time_sleep(payload))
         with open(output_mp3_file, 'wb') as out:
             r = http.request('GET', mp3_file, preload_content=False)
             shutil.copyfileobj(r, out)
